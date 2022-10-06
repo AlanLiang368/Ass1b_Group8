e6246576e8a79124fcf6712411d9616a52d5aee7
AlanLiang368 <allenleung623@gmail.com>
2022-10-06 16:24:06 +0800

    deploy to heroku

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("frontend/build"));
 const articlesRoutes = require("./modules/articles/routes");
 app.use('/api/articles', articlesRoutes);
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
-const port = process.env.PORT || 8082;
+app.use(function (req, res) {
+  const path = require("path");
+  res.sendFile(path.resolve(__dirname, './frontend/build/index.html'));
+})
+const port = process.env.PORT || 8083;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
