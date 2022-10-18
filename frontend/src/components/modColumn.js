78a014b5739ea8bac0f8a3a3cdb8622c4d1b76bc
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2022-10-18 22:35:52 +1300

    mod page
    not functional

---
+++ frontend/src/components/modColumn.js
@@ -0,0 +1,32 @@
+const modColumn = [
+    {
+      Header: "Title",
+      accessor: "title",
+    },
+    {
+      Header: "Authors",
+      accessor: "authors",
+    },
+    {
+      Header: "Source",
+      accessor: "source",
+    },
+    {
+      Header: "Pub. Year",
+      accessor: "publishYear",
+    },
+    {
+      Header: "DOI",
+      accessor: "doi",
+    },
+    {
+      Header: "SePractice",
+      accessor: "sePractice",
+    },
+    {
+        Header: "Action",
+        accessor: "action",
+      },
+  ];
+  
+    module.exports = modColumn;
