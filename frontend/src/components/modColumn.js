74c28392f0eea1e0ace088053e1ef1f9fb42aed0
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2022-10-19 04:22:28 +1300

    moderator css

--- frontend/src/components/modColumn.js
+++ frontend/src/components/modColumn.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const modColumn = [
     {
       Header: "Title",
@@ -30,3 +31,58 @@ const modColumn = [
   ];
   
     module.exports = modColumn;
+=======
+//column for moderators
+import Button from "@mui/material/Button";
+import "../App.css";
+
+const modColumn = [
+  {
+    Header: "Title",
+    accessor: "title",
+  },
+  {
+    Header: "Authors",
+    accessor: "authors",
+  },
+  {
+    Header: "Source",
+    accessor: "source",
+  },
+  {
+    Header: "Pub. Year",
+    accessor: "publishYear",
+  },
+  {
+    Header: "DOI",
+    accessor: "doi",
+  },
+  {
+    Header: "SePractice",
+    accessor: "sePractice",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+    Cell: (row) => (
+      <div className="deleteButton">
+        <Button
+          style={{
+            fontSize: "12px",
+            margin: "4px",
+          }}
+          variant="contained"
+        >
+          Accept
+        </Button>
+        <Button style={{
+            backgroundColor: "#e11e26",
+            fontSize: "12px",
+          }}variant="contained">Reject</Button>
+      </div>
+    ),
+  },
+];
+
+export default modColumn;
+>>>>>>> Stashed changes
